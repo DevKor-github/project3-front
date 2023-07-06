b8a0fde7808bb85a68420655f73f940ad0760fa4
juknow <munjunho77@naver.com>
2023-07-07 04:41:20 +0900

    error

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker , InfoWindow} from '@react-google-maps/api';
 
 
 const containerStyle = {
@@ -11,27 +11,64 @@ function Map({ center, updateCenter }) {
   
   const [localCenter, setLocalCenter] = useState(center);
   const [map, setMap] = useState(null);
-  
-    useEffect(() => {
-      // 유저 현재 위치 정보 받기
-      if (center === null && navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          const initialCenter = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          setLocalCenter(initialCenter);
-          updateCenter(initialCenter);
-        });
-      } else if(center !== localCenter){
-        setLocalCenter(center);
-      }
-    }, [center, updateCenter, localCenter]);
-
+  const [markers, setMarkers] = useState([]);
+ 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyDLOzTtnOt6I8eJjfpxkvzzWNy5AHx3dVM"
   })
+  
+  useEffect(() => {
+    if (center === null && navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const initialCenter = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        setLocalCenter(initialCenter);
+        updateCenter(initialCenter);
+      });
+    } else if(center !== localCenter){
+      setLocalCenter(center);
+    }
+
+    if (isLoaded) {
+      setMarkers([
+        {
+          position: { lat: 37.544709 , lng: 126.968892 },
+          icon: {
+            url: "Cafe.png",
+            scaledSize: new window.google.maps.Size(45.8, 64)
+          },
+          number: 12
+        },
+        {
+          position: { lat: 37.542305 , lng: 126.964535 },
+          icon: {
+            url: "Restaurant.png",
+            scaledSize: new window.google.maps.Size(45.8, 64)
+          },
+          number: 34
+        },
+        {
+          position: { lat: 37.582385 , lng: 127.028430 },
+          icon: {
+            url: "Hotel.png",
+            scaledSize: new window.google.maps.Size(45.8, 64)
+          },
+          number: 4
+        },
+        {
+          position: { lat: 37.587088 , lng: 127.029423 },
+          icon: {
+            url: "Dessert.png",
+            scaledSize: new window.google.maps.Size(45.8, 64)
+          },
+          number: 8
+        }
+      ]);
+    }
+  }, [center, updateCenter, localCenter, isLoaded]);
 
   const onLoad = React.useCallback(function callback(map) {
     if(localCenter) {
@@ -58,34 +95,17 @@ function Map({ center, updateCenter }) {
         options={{ streetViewControl: false, mapTypeControl: false, fullscreenControl: false }}
       >
         {localCenter && customMarker && <Marker position={localCenter} icon={customMarker} />}
-        <Marker
-      icon={{
-        url : "Cafe.png",
-        scaledSize: new window.google.maps.Size(45.8, 64)
-      }}
-      position={{ lat: 37.544709 , lng: 126.968892 }}
-    />
-        <Marker
-      icon={{
-        url : "Restaurant.png",
-        scaledSize: new window.google.maps.Size(45.8, 64)
-      }}
-      position={{ lat: 37.542305 , lng: 126.964535 }}
-    />
-        <Marker
-      icon={{
-        url : "Hotel.png",
-        scaledSize: new window.google.maps.Size(45.8, 64)
-      }}
-      position={{ lat: 37.582385 , lng: 127.028430 }}
-    />
-        <Marker
-      icon={{
-        url : "Dessert.png",
-        scaledSize: new window.google.maps.Size(45.8, 64)
-      }}
-      position={{ lat: 37.587088 , lng: 127.029423 }}
-    />
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            position={marker.position}
+            icon={marker.icon}
+          >
+            <InfoWindow>
+              <div>{marker.number}</div>
+            </InfoWindow>
+          </Marker>
+        ))}
       </GoogleMap>
     </div>
   ) : <></>
