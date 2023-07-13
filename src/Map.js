572315dd7a5e77d5ddc97a3579ce413d42dc3714
juknow <munjunho77@naver.com>
2023-07-13 13:41:25 +0900

    Update Map.js

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { GoogleMap, useJsApiLoader, Marker , InfoWindow, OverlayView} from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 //hello 
 
@@ -10,46 +10,28 @@ const containerStyle = {
 
 function Map() {
   
-  const [localCenter, setLocalCenter] = useState(null);
-  const [center, setCenter] = useState(null);
+  const [localCenter, setLocalCenter] = useState(center);
   const [map, setMap] = useState(null);
   const [markers, setMarkers] = useState([]);
   const updateCenter = (newCenter) => {  
     setCenter(newCenter);
   }
   
-  useEffect(() => {
-    // 유저 현재 위치 정보 받기
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        setCenter({
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
+    useEffect(() => {
+      // 유저 현재 위치 정보 받기
+      if (center === null && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const initialCenter = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          setLocalCenter(initialCenter);
+          updateCenter(initialCenter);
         });
-      });
-    } else {
-      console.log("브라우저가 위치 정보를 지원하지 않습니다.");
-    }
-  }, []);
-
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "AIzaSyDLOzTtnOt6I8eJjfpxkvzzWNy5AHx3dVM"
-  })
-  
-  useEffect(() => {
-    if (center === null && navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const initialCenter = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        setLocalCenter(initialCenter);
-        updateCenter(initialCenter);
-      });
-    } else if(center !== localCenter){
-      setLocalCenter(center);
-    }
+      } else if(center !== localCenter){
+        setLocalCenter(center);
+      }
+    }, [center, updateCenter, localCenter]);
 
     if (isLoaded) {
       setMarkers([
@@ -143,49 +125,34 @@ function Map() {
         options={{ streetViewControl: false, mapTypeControl: false, fullscreenControl: false }}
       >
         {localCenter && customMarker && <Marker position={localCenter} icon={customMarker} />}
-        {markers.map((marker, index) => (
-          <Marker
-            key={index}
-            position={marker.position}
-            icon={marker.icon}
-          >
-            <InfoWindow>
-              <div>{marker.number}</div>
-            </InfoWindow>
-          </Marker>
-        ))}
-        <LabeledMarker
-    label="14"
-    icon={{
-      url : "Cafe.png",
-      scaledSize: new window.google.maps.Size(45.8, 64)
-    }}
-    position={{ lat: 37.544709 , lng: 126.968892 }}
-  />
-        <LabeledMarker
-    label="83"
-    icon={{
-      url : "Restaurant.png",
-      scaledSize: new window.google.maps.Size(45.8, 64)
-    }}
-    position={{ lat: 37.542305 , lng: 126.964535 }}
-  />
-        <LabeledMarker
-    label="9"
-    icon={{
-      url : "Hotel.png",
-      scaledSize: new window.google.maps.Size(45.8, 64)
-    }}
-    position={{ lat: 37.582385 , lng: 127.028430 }}
-  />
-          <LabeledMarker
-    label="113"
-    icon={{
-      url : "Dessert.png",
-      scaledSize: new window.google.maps.Size(45.8, 64)
-    }}
-    position={{ lat: 37.587088 , lng: 127.029423 }}
-  />
+        <Marker
+      icon={{
+        url : "Cafe.png",
+        scaledSize: new window.google.maps.Size(45.8, 64)
+      }}
+      position={{ lat: 37.544709 , lng: 126.968892 }}
+    />
+        <Marker
+      icon={{
+        url : "Restaurant.png",
+        scaledSize: new window.google.maps.Size(45.8, 64)
+      }}
+      position={{ lat: 37.542305 , lng: 126.964535 }}
+    />
+        <Marker
+      icon={{
+        url : "Hotel.png",
+        scaledSize: new window.google.maps.Size(45.8, 64)
+      }}
+      position={{ lat: 37.582385 , lng: 127.028430 }}
+    />
+        <Marker
+      icon={{
+        url : "Dessert.png",
+        scaledSize: new window.google.maps.Size(45.8, 64)
+      }}
+      position={{ lat: 37.587088 , lng: 127.029423 }}
+    />
       </GoogleMap>
     </div>
   ) : <></>
